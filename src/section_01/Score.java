ff0e0d16b4d73c94e6edd288acc46451c69508ec
MinJun <mj061101k@naver.com>
2023-05-05 21:44:36 +0900

    2023-05-04(수정01)

--- src/section_01/Score.java
+++ src/section_01/Score.java
@@ -6,6 +6,7 @@ public class Score {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int score = sc.nextInt();
+        sc.close();
         
         if (score >= 90) {
             System.out.println("A");
